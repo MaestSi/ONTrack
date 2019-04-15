c9f3bebe71cee96f9699c3fecfdd7500cfe353e8
Simone Maestri <simone.maestri@hotmail.it>
2019-04-15 09:51:46 +0200

    Update MinION_mobile_lab.R

--- MinION_mobile_lab.R
+++ MinION_mobile_lab.R
@@ -152,14 +152,15 @@ cat(text = "\n")
 cat(text = paste0("Basecalling started at ", date()), file = logfile, sep = "\n", append = TRUE)
 cat(text = paste0("Basecalling started at ", date()), sep = "\n")
 
+num_threads_caller <- round(num_threads/4)
 if (flip_flop_flag==1) {
-  system(paste0(basecaller, " -r -i ", d1, " --cpu_threads_per_caller ", num_threads, " -c dna_r9.4.1_450bps_flipflop.cfg --hp_correct TRUE --fast5_out -s ", d2_basecalling))
+  system(paste0(basecaller, " -r -i ", d1, " --cpu_threads_per_caller ", num_threads_caller, " -c dna_r9.4.1_450bps_flipflop.cfg --hp_correct TRUE --fast5_out -s ", d2_basecalling))
 } else {
-  system(paste0(basecaller, " -r -i ", d1, " --cpu_threads_per_caller ", num_threads, " --flowcell ", flowcell, " --kit ", kit, " --hp_correct TRUE --fast5_out -s ", d2_basecalling))
+  system(paste0(basecaller, " -r -i ", d1, " --cpu_threads_per_caller ", num_threads_caller, " --flowcell ", flowcell, " --kit ", kit, " --hp_correct TRUE --fast5_out -s ", d2_basecalling))
 }
 
 if (pair_strands_flag==1) {
-  system(paste0(basecaller_1d2, " -r -i ", d2_basecalling, "/workspace --cpu_threads_per_caller ", num_threads, " --config dna_r9.5_450bps_1d2_raw.cfg -f ", d2_basecalling, "/sequencing_summary.txt -s ", d2, "/basecalling_1d2"))
+  system(paste0(basecaller_1d2, " -r -i ", d2_basecalling, "/workspace --cpu_threads_per_caller ", num_threads_caller, " --config dna_r9.5_450bps_1d2_raw.cfg -f ", d2_basecalling, "/sequencing_summary.txt -s ", d2, "/basecalling_1d2"))
   d2_basecalling <- paste0(d2, "/basecalling_1d2")
 }
 
@@ -170,8 +171,8 @@ cat(text = "\n")
 
 cat(text = paste0("Demultiplexing started at ", date()), file = logfile, sep = "\n", append = TRUE)
 cat(text = paste0("Demultiplexing started at ", date()), sep = "\n")
-#system(paste0(demultiplexer, " -r -i ", d2_basecalling, " -t ", num_threads, " -s ", d2_preprocessing, " --kit ", kit, " --barcode_kits ", barcode_kits))
-system(paste0(demultiplexer, " -r -i ", d2_basecalling, " -t ", num_threads, " -s ", d2_preprocessing))
+system(paste0(demultiplexer, " -r -i ", d2_basecalling, " -t ", num_threads, " -s ", d2_preprocessing, " --kit ", kit, " --barcode_kits ", barcode_kits))
+#system(paste0(demultiplexer, " -r -i ", d2_basecalling, " -t ", num_threads, " -s ", d2_preprocessing))
 cat(text = paste0("Demultiplexing finished at ", date()), file = logfile, sep = "\n", append = TRUE)
 cat(text = paste0("Demultiplexing finished at ", date()), sep = "\n")
 cat(text = "\n", file = logfile, append = TRUE)
