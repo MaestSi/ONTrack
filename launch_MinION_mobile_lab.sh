e488789446812c4296399921d60c6bf5894173be
Simone Maestri <simone.maestri@hotmail.it>
2019-04-17 10:09:45 +0200

    Update launch_MinION_mobile_lab.sh

--- launch_MinION_mobile_lab.sh
+++ launch_MinION_mobile_lab.sh
@@ -20,6 +20,6 @@
 
 RAW_READS_DIR=$1
 RAW_READS_DIR_FULL=$(realpath $RAW_READS_DIR)
-source activate ONTrack_env
-pipeline_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+conda activate ONTrack_env
+pipeline_dir=$( realpath $(dirname "${BASH_SOURCE[0]}" ))
 nohup Rscript $pipeline_dir/MinION_mobile_lab.R $pipeline_dir/config_MinION_mobile_lab.R $RAW_READS_DIR_FULL &
