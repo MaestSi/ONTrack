5e30f1821fb9b12bbedfa1ff2ed25d6c998be6f3
Simone Maestri <simone.maestri@hotmail.it>
2019-06-06 18:02:35 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ MINICONDA_DIR=$(which conda | sed 's/bin.*$//')
 conda config --add channels bioconda
 conda config --add channels conda-forge
 conda config --add channels r
-conda create -n ONTrack_env python=3.6 blast emboss vsearch seqtk mafft porechop minimap2 samtools nanopolish r bioconductor-biostrings 
+conda create -n ONTrack_env python=3.6 blast emboss vsearch seqtk mafft porechop minimap2 samtools nanopolish bedtools r bioconductor-biostrings 
 source activate ONTrack_env
 pip install pycoQC
 echo -e "\n"
