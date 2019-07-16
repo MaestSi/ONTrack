4699a191dd70f4cafe1717db07ef7416592814dd
Simone Maestri <simone.maestri@hotmail.it>
2019-07-16 22:30:07 +0200

    Update config_MinION_mobile_lab.R

--- config_MinION_mobile_lab.R
+++ config_MinION_mobile_lab.R
@@ -24,7 +24,7 @@ do_subsampling_flag <- 0
 num_fast5_files <- 50000
 #BC_int are the barcodes used in the experiment
 #BC_int <- c("BC01", "BC02", "BC03", "BC04", "BC05", "BC06", "BC07", "BC08", "BC09", "BC10", "BC11", "BC12")
-BC_int <- c("BC01", "BC02", "BC03", "BC04", "BC05", "BC06", "BC07", "BC09", "BC10", "BC11", "BC12")
+BC_int <- c("BC01", "BC02", "BC03", "BC04", "BC05", "BC06", "BC07", "BC08", "BC09", "BC10", "BC11", "BC12")
 #barcode kits
 #barcode_kits <- c("EXP-NBD103", "EXP-NBD114", "EXP-PBC001", "EXP-PBC096", "SQK-16S024", "SQK-LWB001", "SQK-RAB201", "SQK-RBK001", "SQK-RBK004", "SQK-RLB001")
 barcode_kits <- c("EXP-PBC001")
